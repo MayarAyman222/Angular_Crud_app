a89b37800d73524d429b4e526812c4a0dbca176d
MayarAyman222 <143541857+MayarAyman222@users.noreply.github.com>
2025-09-21 00:49:12 +0300

    Update edit.component.ts

--- src/app/edit_user/edit/edit.component.ts
+++ src/app/edit_user/edit/edit.component.ts
@@ -1,40 +1,4 @@
-/*import { Component , OnInit} from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-interface User{
-  id: number,
-  name : string,
-  email:string,
-  title:string,
-  body:string
-};
-@Component({
-  selector: 'app-edit',
-  imports: [FormsModule ],
-  templateUrl: './edit.component.html',
-  styleUrl: './edit.component.css'
-})
 
-export class EditComponent {
-user!:User
-constructor(public router:Router , public route:ActivatedRoute){}
-ngOnInit():void{
-  let userid =Number(this.route.snapshot.paramMap.get("id")||0);
-  let users:User []=JSON.parse(localStorage.getItem("users")||"[]")
- 
-    this.user = users.find((user: User) => user.id == userid)!;
-}
- saveChanges() {
-    // دورنا على index بتاع الـ user اللي بيتعدل
-    const index = this.users.findIndex(u => u.id === this.user.id);
-
-    if (index !== -1) {
-      this.users[index] = this.user; // حفظ التعديلات
-      localStorage.setItem('users', JSON.stringify(this.users));
-    }
-
-}
-*/
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
